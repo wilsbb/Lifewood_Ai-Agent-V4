e7c8ed8991f16272ec302bbdc24534bdd367e460
FrancisGarryNillama <paenggwapokaayo123@gmail.com>
2026-04-08 15:40:50 +0800

    Implemented export functionality of the entirety of the raw data

--- expense-ai-backend/expense_ai/settings.py
+++ expense-ai-backend/expense_ai/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'google_drive',
     'corsheaders',
     'billing',
