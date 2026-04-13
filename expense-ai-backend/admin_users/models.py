296d20f70968c49ce16dc9fa4c8a5b25f8747e05
FrancisGarryNillama <paenggwapokaayo123@gmail.com>
2026-04-13 11:29:29 +0800

    Fixed auth bug

--- expense-ai-backend/admin_users/models.py
+++ expense-ai-backend/admin_users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 class AdminUserProfile(models.Model):
